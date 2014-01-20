646ce79c3902dd332674ef6e56c00982979f85ff
Jimmy Latreille <jimmylatreille@gmail.com>
2014-01-19 23:51:15 -0500

    my first lib commit

---
+++ README.md
@@ -0,0 +1,4 @@
+lib
+===
+
+Javascript librairy
---
+++ lib.js
@@ -0,0 +1,14 @@
+/*LIB version 0.1
+*	 @author Jimmy Latreille
+*	 @date 19 janvier 2014 23pm
+*	 @email jimmylatreille@gmail.com
+*	 
+*	 @Description LIB 
+*	 Javascript librairy POO
+*/
+
+var log = console.log;
+
+ const LIB  = {
+	version : 0.1
+}
