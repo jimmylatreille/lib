4a979303fe455123db84fded54559807b7722b4b
Jimmy Latreille <jimmy.latreille@emcitv.com>
2022-03-20 11:32:13 -0400

    user

---
+++ User.php
@@ -0,0 +1,27 @@
+<?php 
+
+class User {
+
+	//variable instance
+	private $name;
+
+	//constructeur
+	function __construct($name){
+
+		$this->name = $name;
+		
+	} //fin constructeur User
+
+	//Getter name()
+	 public function getName(){
+		return $this->name;
+	}
+
+	//Setter name()
+	 public function setName($name){
+		$this->name = $name;
+	} 
+	
+} // fin de la class User
+
+ ?>
